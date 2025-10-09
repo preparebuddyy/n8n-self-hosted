abd67aa6f317adc72de40a22be18c5b65d41d847
AiratTop <airat.halitov@gmail.com>
2025-10-09 03:44:27 +0300

    Format Caddyfile

--- caddy-reload.sh
+++ caddy-reload.sh
@@ -2,5 +2,8 @@
 
 date
 
+# Format Caddyfile
+docker compose exec caddy caddy fmt --overwrite --config /etc/caddy/Caddyfile
+
 # Gracefully reload the Caddy configuration without downtime.
 docker compose exec caddy caddy reload --config /etc/caddy/Caddyfile
